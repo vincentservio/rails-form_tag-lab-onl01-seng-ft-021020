d903e61d92003328fc6a2b7332390c14575975f3
vincentservio <vincentservio@gmail.com>
2020-04-15 14:55:57 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,7 +5,21 @@ class StudentsController < ApplicationController
   end
 
   def show
-    @student = Student.find(params[:id])
+      @student = Student.find_by(id:params[:id])
+      if @student 
+        render "show"
+      else
+        redirect_to students_path
+      end
   end
+
+  def new
+  
+end
+
+def create
+  Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+  redirect_to students_path
+end
 
 end
